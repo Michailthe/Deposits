65f7a2760bfd14c4c16eb023fcd9fb62aadb9ed6
Michailthe <112062292+Michailthe@users.noreply.github.com>
2024-10-19 21:58:29 +0300

    Добавьте файлы проекта.

---
+++ Вклады/Вклады.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    int  Contribution;
+    int Theinterestrate, thedesiredamount, waityears = 0;
+    cout << "Enter the deposit amount. " << endl;
+    cin >> Contribution;
+    cout << " Enter the interest rate" << endl;
+    cin >> Theinterestrate;
+    cout << " Enter the desired amount" << endl;
+    cin >> thedesiredamount;
+    while (Contribution < thedesiredamount)
+    {
+        Contribution += Contribution * Theinterestrate / 100;
+        waityears;
+    }
+    cout << "We`ll have to wait " << waityears << "years" << endl;
+
+}
